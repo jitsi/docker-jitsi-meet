463bf716f05aac14e41a7eec130c29fdb2e58a30
netaskd <netaskd@gmail.com>
2019-12-12 18:42:10 +0200

    turn: the fist review changes

--- turn/rootfs/defaults/docker-entrypoint.sh
+++ turn/rootfs/defaults/docker-entrypoint.sh
@@ -1,14 +1,24 @@
 #!/bin/ash
+# create config dir if not exists
+[ ! -d /config ] && mkdir /config
+
 # make certs if not exist
-if [[ ! -f /etc/ssl/cert.crt || ! -f /etc/ssl/cert.key  ]]; then
-  openssl req -newkey rsa:2048 -nodes -keyout key.pem -x509 -days 3650 -out certificate.pem -subj "/C=US/ST=NY/L=NY/O=IT/CN=${TURN_HOST}"
+if [[ ! -f /config/cert.crt || ! -f /config/cert.key  ]]; then
+  openssl req -newkey rsa:2048 -nodes -keyout /config/cert.key -x509 -days 3650 -out /config/cert.crt -subj "/C=US/ST=NY/L=NY/O=IT/CN=${TURN_HOST}"
 fi
 
-# set coturn admin user
-turnadmin -A -u ${TURN_ADMIN_USER:-admin} -p ${TURN_ADMIN_SECRET:-changeme}
+# use non empty TURN_PUBLIC_IP variable, othervise set it dynamically.
+[ -z "${TURN_PUBLIC_IP}" ] && export TURN_PUBLIC_IP=$(curl -4ks https://icanhazip.com)
+[ -z "${TURN_PUBLIC_IP}" ] && echo "ERROR: variable TURN_PUBLIC_IP is not set and can not be set dynamically!" && kill 1
+
+# set coturn web-admin access
+if [[ "${TURN_ADMIN_ENABLE}" == "1" || "${TURN_ADMIN_ENABLE}" == "true" ]]; then
+  turnadmin -A -u ${TURN_ADMIN_USER:-admin} -p ${TURN_ADMIN_SECRET:-changeme}
+  export TURN_ADMIN_OPTIONS="--web-admin --web-admin-ip=$(hostname -i) --web-admin-port=${TURN_ADMIN_PORT:-8443}"
+fi
 
 # run coturn server with API auth method enabled.
-turnserver -n \
+turnserver -n ${TURN_ADMIN_OPTIONS} \
 --verbose \
 --prod \
 --no-tlsv1 \
@@ -18,8 +28,8 @@ turnserver -n \
 --tls-listening-port=${TURN_PORT:-5349} \
 --alt-listening-port=${TURN_PORT:-5349} \
 --alt-tls-listening-port=${TURN_PORT:-5349} \
---cert=/etc/ssl/cert.crt \
---pkey=/etc/ssl/cert.key \
+--cert=/config/cert.crt \
+--pkey=/config/cert.key \
 --min-port=${TURN_RTP_MIN:-10000} \
 --max-port=${TURN_RTP_MAX:-11000} \
 --no-stun \
@@ -27,12 +37,8 @@ turnserver -n \
 --static-auth-secret=${TURN_SECRET:-keepthissecret} \
 --no-multicast-peers \
 --realm=${TURN_REALM:-realm} \
---external-ip=$(curl -4k https://icanhazip.com 2>/dev/null) \
---relay-ip=$(hostname -i) \
 --listening-ip=$(hostname -i) \
---web-admin \
---web-admin-ip=$(hostname -i) \
---web-admin-port=${TURN_ADMIN_PORT:-8443} \
---no-cli \
---cli-password=${TURN_ADMIN_SECRET:-changeme}
+--external-ip=${TURN_PUBLIC_IP} \
+--cli-password=NotReallyCliUs3d \
+--no-cli
 
