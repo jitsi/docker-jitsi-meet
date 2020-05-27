fd2c68966ab54c643b7beaaf264b99ca7ea631c0
Rustam Akhmedov <rustam.akhmedov@gmail.com>
2020-05-27 18:52:31 +0500

    Changes: - using custom sources for `jitsi-meet` and `jicofo` - jibri ports exposed

--- build-custom-images.sh
+++ build-custom-images.sh
@@ -1,16 +1,62 @@
 #!/bin/bash
 
-echo "Building custom jitsi/web"
-cd web
-docker build --tag jitsi/web:custom .
-cd ..
-
 envFile=".env"
+
+# To use multiple deploy keys for diff repos we used alias names for github repos.
+# https://stackoverflow.com/questions/10041082/how-to-add-deploy-key-for-2-repo-with-1-user-on-github
 
 if [ -f $envFile ]; then
   set -a
   source $envFile
 
+  echo "Building custom jitsi/web"
+  if [ -d "$CUSTOM_TRS_JITSI_MEET_BRANCH" ]; then
+    cd web
+    echo "Getting latest custom jitsi-meet codebase"
+    if [ ! -d "trs-jitsi-meet" ]; then
+      mkdir trs-jitsi-meet
+    fi
+    cd trs-jitsi-meet
+    if [ -d .git ]; then
+      git clone github-trs-jitsi-meet:TheRealStart/jitsi-meet.git .
+    else
+      git fetch
+      git checkout "$CUSTOM_TRS_JITSI_MEET_BRANCH"
+      git pull
+    fi
+    npm install
+    make
+    make source-package
+    cd ..
+    tar -xzf trs-jitsi-meet/jitsi-meet.tar.bz2
+    docker build --tag jitsi/web:custom .
+    cd ..
+  fi
+
+  if [ -d "$CUSTOM_TRS_JICOFO_BRANCH" ]; then
+    cd jicofo
+    echo "Getting latest custom jicofo codebase"
+    if [ ! -d "trs-jicofo" ]; then
+      mkdir trs-jicofo
+    fi
+    cd trs-jicofo
+    if [ -d .git ]; then
+      git clone github-trs-jicofo:TheRealStart/jicofo.git .
+    else
+      git fetch
+      git checkout "$CUSTOM_TRS_JICOFO_BRANCH"
+      git pull
+    fi
+    mvn package -DskipTests -Dassembly.skipAssembly=false
+    cd ..
+    unzip trs-jicofo/target/jicofo-1.1-SNAPSHOT-archive.zip
+    # compiled files are located under folder: trs-jicofo/jicofo-1.1-SNAPSHOT
+    echo "Building custom jicofo"
+    docker build --tag jitsi/jicofo:custom .
+    cd ..
+  fi
+
+  # Copying prosody moderation plugin to configs path
   if [ -d "$CONFIG" ]; then
     if [ ! -d "$CONFIG/prosody/prosody-plugins-custom" ]; then
       mkdir "$CONFIG/prosody/prosody-plugins-custom"
