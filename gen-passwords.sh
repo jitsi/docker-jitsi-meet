075c5a77bf8329c1546615bdae525aae88272ab1
Jarryd  Lisher <jarryd@jlisher.com>
2022-05-12 21:30:18 +0200

    Add secrets generator function

--- gen-passwords.sh
+++ gen-passwords.sh
@@ -4,6 +4,29 @@ function generatePassword() {
     openssl rand -hex 16
 }
 
+function generateSecrets() {
+    local -r secrets_dir="${0%/*}/.secrets"
+    local -ar secrets=(
+        "CALLSTATS_SECRET"
+        "JIBRI_RECORDER_PASSWORD"
+        "JIBRI_XMPP_PASSWORD"
+        "JICOFO_AUTH_PASSWORD"
+        "JICOFO_COMPONENT_SECRET"
+        "JIGASI_XMPP_PASSWORD"
+        "JVB_AUTH_PASSWORD"
+        "JWT_APP_SECRET"
+    )
+
+    [[ -d "${secrets_dir}" ]] || mkdir -p "${secrets_dir}"
+
+    for name in "${secrets[@]}"; do
+        # skip if exists.
+        ! [[ -f "${secrets_dir}/${name}" ]] || continue
+
+        generatePassword > "${secrets_dir}/${name}"
+    done
+}
+
 JICOFO_AUTH_PASSWORD=$(generatePassword)
 JVB_AUTH_PASSWORD=$(generatePassword)
 JIGASI_XMPP_PASSWORD=$(generatePassword)
