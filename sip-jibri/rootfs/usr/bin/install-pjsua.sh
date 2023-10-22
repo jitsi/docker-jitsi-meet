70a8523507170f5f90e8634270414255e8cbb30f
emrah <emrah.eryilmaz@nordeck.net>
2023-10-22 15:13:00 +0300

    sip-jibri: install pjsua

---
+++ sip-jibri/rootfs/usr/bin/install-pjsua.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -o pipefail -xeu
+
+mv /opt/jitsi/jibri/pjsua.custom.sh /opt/jitsi/jibri/pjsua.sh
+mv /opt/jitsi/jibri/finalize_sip.custom.sh /opt/jitsi/jibri/finalize_sip.sh
+
+# Fix this when pjsua is avilable as deb package
+curl -4Lso /usr/local/bin/pjsua https://emrah.com/files/pjsua_2.10-jitsi3
+chmod 755 /usr/local/bin/pjsua
+
+pjsua --version
