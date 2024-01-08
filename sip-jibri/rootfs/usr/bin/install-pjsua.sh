17c1fbc080f49d259563e03b383aa92d8b5a1a7e
emrah <emrah.eryilmaz@nordeck.net>
2024-01-08 18:39:13 +0300

    sip-jibri: use an intermediate image to build pjsua

--- sip-jibri/rootfs/usr/bin/install-pjsua.sh
+++ sip-jibri/rootfs/usr/bin/install-pjsua.sh
@@ -6,8 +6,6 @@ mv /opt/jitsi/jibri/pjsua.custom.sh /opt/jitsi/jibri/pjsua.sh
 mv /opt/jitsi/jibri/finalize_sip.custom.sh /opt/jitsi/jibri/finalize_sip.sh
 mv /etc/jitsi/jibri/pjsua.custom.config /etc/jitsi/jibri/pjsua.config
 
-# Fix this when pjsua is avilable as deb package
-curl -4Lso /usr/local/bin/pjsua https://emrah.com/files/pjsua_2.10-jitsi3
 chmod 755 /usr/local/bin/pjsua
 
 pjsua --version
