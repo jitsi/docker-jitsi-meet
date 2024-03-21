52cf6cfbcbc0b4afc054b3d4de26a96ed23320e1
damencho <damencho@jitsi.org>
2024-03-21 06:40:51 -0500

    feat(prosody): Adjusts lua to use generational GC.

--- prosody/rootfs/defaults/prosody.cfg.lua
+++ prosody/rootfs/defaults/prosody.cfg.lua
@@ -4,7 +4,7 @@
 {{ $ENABLE_VISITORS := .Env.ENABLE_VISITORS | default "0" | toBool -}}
 {{ $ENABLE_S2S := or $ENABLE_VISITORS ( .Env.PROSODY_ENABLE_S2S | default "0" | toBool ) }}
 {{ $ENABLE_IPV6 := .Env.ENABLE_IPV6 | default "true" | toBool -}}
-{{ $GC_TYPE := .Env.GC_TYPE | default "incremental" -}}
+{{ $GC_TYPE := .Env.GC_TYPE | default "generational" -}}
 {{ $GC_INC_TH := .Env.GC_INC_TH | default 150 -}}
 {{ $GC_INC_SPEED := .Env.GC_INC_SPEED | default 250 -}}
 {{ $GC_INC_STEP_SIZE := .Env.GC_INC_STEP_SIZE | default 13 -}}
