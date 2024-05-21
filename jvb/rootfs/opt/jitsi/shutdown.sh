cacee4ecc817361905f04930f24ff2273502972a
Aaron van Meerten <aaron.van.meerten@gmail.com>
2024-05-21 16:09:20 -0500

    jvb: adds autoscaler sidecar hook to signal final shutdown (#1809)
    * jvb: adds autoscaler sidecar hook to signal final shutdown
    
    * cleaner ports

--- jvb/rootfs/opt/jitsi/shutdown.sh
+++ jvb/rootfs/opt/jitsi/shutdown.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/with-contenv bash
+# notify the sidecar of imminent shutdown
+PORT=${AUTOSCALER_SIDECAR_PORT:-6000}
+curl -d '{}' -v 0:$PORT/hook/v1/shutdown
+sleep 10
 
 # shutdown everything
 s6-svscanctl -t /var/run/s6/services
