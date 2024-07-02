c65d2bd2fca045f10a625040a93a136fbf2684fb
Sebastian Denz <denzs@gonicus.de>
2024-07-02 17:50:25 +0200

    feat(jigasi,jicofo,jvb,jibri,prosody): add healthcheck.sh scripts and prosody http_health mod

--- prosody/rootfs/defaults/prosody.cfg.lua
+++ prosody/rootfs/defaults/prosody.cfg.lua
@@ -99,6 +99,7 @@ modules_enabled = {
 		--"watchregistrations"; -- Alert admins of registrations
 		--"motd"; -- Send a message to users when they log in
 		--"legacyauth"; -- Legacy authentication. Only used by some old clients and bots.
+		"http_health";
 		{{ if eq .Env.PROSODY_MODE "brewery" -}}
 		"firewall"; -- Enable firewalling
 		"secure_interfaces";
