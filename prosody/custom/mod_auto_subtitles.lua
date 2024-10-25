3ec98ac83413387756be8af4a3fc6fea6b5935a1
Brice Aminou <brice.aminou@gmail.com>
2024-10-26 00:00:45 +0200

    fix

--- prosody/custom/mod_auto_subtitles.lua
+++ prosody/custom/mod_auto_subtitles.lua
@@ -1,14 +1,24 @@
+-- File: /prosody-plugins/mod_auto_subtitles.lua
+
+local st = require "util.stanza"
+
 module:hook("muc-room-created", function(event)
+    module:log("info", "Auto subtitles module triggered for new room.")
     local room = event.room
+
+    -- Function to start transcription
     local start_transcription = function()
-        -- Send a custom message to start transcription
-        room:send_message({
-            name = "transcription",
-            type = "command",
-            value = "start",
-            sender = "jigasi@your.jitsi.domain"
-        })
+        module:log("info", "Starting transcription for room: %s", room.jid)
+
+        -- Create a message stanza to start transcription
+        local message = st.message({ type = "groupchat", from = room.jid })
+            :tag("body"):text("!starttranscription"):up()
+            :tag("nick", { xmlns = "http://jabber.org/protocol/nick" }):text("jigasi")
+
+        -- Send the message to the room
+        room:route_stanza(message)
     end
+
     -- Schedule the transcription start
     module:add_timer(1, start_transcription)
 end)
