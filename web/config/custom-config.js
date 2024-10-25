c81e5f8960b0db10beede4e17848a8e762a88f48
Brice Aminou <brice.aminou@gmail.com>
2024-10-25 16:07:41 +0200

    fix

--- web/config/custom-config.js
+++ web/config/custom-config.js
@@ -27,3 +27,5 @@ config.toolbarButtons = [
 
 config.disableDeepLinking = true;
 config.transcription.enabled = true
+config.transcription.useAppLanguage = false;
+config.transcription.preferredLanguage = 'en-US';
