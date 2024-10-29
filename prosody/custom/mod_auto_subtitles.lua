1a8c415d682d51c54ff2c87f1e609c2f5eb99850
Brice Aminou <brice.aminou@gmail.com>
2024-10-29 21:02:28 +0100

    fix

--- prosody/custom/mod_auto_subtitles.lua
+++ prosody/custom/mod_auto_subtitles.lua
@@ -1,33 +1,25 @@
-local st = require "util.stanza"
+-- module: mod_auto_jigasi.lua
+local jid_split = require "util.jid".split;
+local st = require("util.stanza");
+
+local muc_domain_base = module:get_option_string("muc_mapper_domain_base");
+local jigasi_bare_jid = module:get_option_string("muc_jigasi_jid", "jigasi@auth." .. muc_domain_base);
+local focus_jid = module:get_option_string("muc_jicofo_brewery_jid", "focus@" .. muc_domain_base);
+
+local function invite_jigasi_to_room(room_jid)
+    local room_name = jid_split(room_jid);
+    local jigasi_jid = jigasi_bare_jid;
+    local stanza_id = "jigasi-" .. tostring(os.time());
+    
+    local invite_stanza = st.iq({ xmlns = "jabber:client", type = "set", to = jigasi_jid, from = focus_jid, id = stanza_id })
+        :tag("dial", { xmlns = "urn:xmpp:rayo:1", from = "transcription", to = "transcription" })
+        :tag("header", { xmlns = "urn:xmpp:rayo:1", name = "JvbRoomName", value = room_name });
+        
+    module:log("info", "Automatically inviting Jigasi for transcription in room %s", room_name);
+    module:send(invite_stanza);
+end
 
 module:hook("muc-room-created", function(event)
-    module:log("info", "Auto subtitles module triggered for new room.")
-    local room = event.room
-
-    -- Ensure room object exists
-    if not room then
-        module:log("error", "Room object is nil.")
-        return
-    end
-
-    -- Function to start subtitles
-    local start_subtitles = function()
-        module:log("info", "Attempting to start subtitles for room: %s", room.jid)
-
-        -- Create a message stanza to start subtitles
-        local message = st.message({ type = "groupchat", from = room.jid })
-            :tag("body"):text("!startsubtitles"):up()
-            :tag("nick", { xmlns = "http://jabber.org/protocol/nick" }):text("jigasi")
-
-        -- Send the message to the room
-        local success, err = pcall(function() room:route_stanza(message) end)
-        if not success then
-            module:log("error", "Failed to send subtitles start message: %s", err)
-        else
-            module:log("info", "Subtitles start message sent successfully.")
-        end
-    end
-
-    -- Schedule the subtitles start
-    module:add_timer(1, start_subtitles)
-end)
+    local room = event.room;
+    invite_jigasi_to_room(room.jid);
+end);
