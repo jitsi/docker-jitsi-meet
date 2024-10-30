d09c2ae2e55823ce5cb06781fc12bdefbdd0ab65
Brice Aminou <brice.aminou@gmail.com>
2024-10-30 16:10:05 +0100

    fix

--- prosody/custom/mod_transcriber_autostart.lua
+++ prosody/custom/mod_transcriber_autostart.lua
@@ -1,16 +1,66 @@
--- module: mod_muc_auto_subtitles.lua
-local jid_split = require "util.jid".split;
-local st = require("util.stanza");
+local LOGLEVEL = "info"
 
-local muc_domain_base = module:get_option_string("muc_mapper_domain_base");
-local jigasi_bare_jid = module:get_option_string("muc_jigasi_jid", "jigasi@auth." .. muc_domain_base);
-local focus_jid = module:get_option_string("muc_jicofo_brewery_jid", "focus@" .. muc_domain_base);
+local is_admin = require "core.usermanager".is_admin
+local is_healthcheck_room = module:require "util".is_healthcheck_room
+local timer = require "util.timer"
+local st = require "util.stanza"
+local uuid = require "util.uuid".generate
+module:log(LOGLEVEL, "loaded")
 
-local function invite_jigasi_to_room(room_jid)
-    module:log("This starts for Brice")
+-- -----------------------------------------------------------------------------
+local function _is_admin(jid)
+    return is_admin(jid, module.host)
 end
 
-module:hook("muc-occupant-joined", function(event)
-    local room = event.room;
-    invite_jigasi_to_room(room.jid);
-end);
+-- -----------------------------------------------------------------------------
+local function _start_recording(room, session, occupant_jid)
+    -- dont start recording if already triggered
+    if room.is_recorder_triggered then
+        return
+    end
+
+    -- get occupant current status
+    local occupant = room:get_occupant_by_real_jid(occupant_jid)
+
+    -- check recording permission
+    if occupant.role ~= "moderator" then
+        return
+    elseif
+        session.jitsi_meet_context_features ~= nil and
+        session.jitsi_meet_context_features["recording"] ~= true
+    then
+        return
+    end
+
+    -- start recording
+    local iq = st.iq({
+        type = "set",
+        id = uuid() .. ":sendIQ",
+        from = occupant_jid,
+        to = room.jid .. "/focus"
+        })
+        :tag("jibri", {
+            xmlns = "http://jitsi.org/protocol/jibri",
+            action = "start",
+            recording_mode = "file",
+            app_data = '{"file_recording_metadata":{"share":true}}'})
+
+    module:send(iq)
+    room.is_recorder_triggered = true
+end
+
+-- -----------------------------------------------------------------------------
+module:hook("muc-occupant-joined", function (event)
+    local room = event.room
+    local session = event.origin
+    local occupant = event.occupant
+
+    if is_healthcheck_room(room.jid) or _is_admin(occupant.jid) then
+        return
+    end
+
+    -- wait for the affiliation to set then start recording if applicable
+    timer.add_task(3, function()
+        _start_recording(room, session, occupant.jid)
+    end)
+end)
