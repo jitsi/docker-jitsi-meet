ee1f55b874ab8190eb21bdb3e4c1261189323230
Brice Aminou <brice.aminou@gmail.com>
2024-10-30 18:04:48 +0100

    fix

--- prosody/custom/mod_transcriber_autostart.lua
+++ prosody/custom/mod_transcriber_autostart.lua
@@ -17,20 +17,17 @@ local function _start_recording(room, session, stanza)
     -- Customize Jigasi JID to the one set up in your environment
     local jigasi_jid = "jigasi@example.com"; -- replace with Jigasi's actual JID
 
-    module:log("info",room.jid)
-    module:log("info", jigasi_jid)
-    module:log("info","Hello!!!")
-    if not room then
-        return
-    end
-
     -- Invite Jigasi to the room to start transcription
     module:log("info", "Inviting Jigasi for transcription to room: %s", room.jid);
-    -- room:send(st.presence({ from = jigasi_jid, to = room.jid })
-    --     :tag("x", { xmlns = "http://jabber.org/protocol/muc" }));
+    local jigasi_presence = st.presence({ from = jigasi_jid, to = room.jid })
+        :tag("x", { xmlns = "http://jabber.org/protocol/muc" })
 
-    -- Optionally send a message to indicate transcription has started
-    room:broadcast_message("Transcription service has been activated for this room.");
+    room:route_stanza(jigasi_presence)  -- Use route_stanza instead of send
+
+    -- Optionally, send a message indicating transcription has started
+    local message = st.message({ type="groupchat", from = jigasi_jid, to = room.jid })
+        :tag("body"):text("Transcription service has been activated for this room.")
+    room:route_stanza(message)
     return
 end
 
