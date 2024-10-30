2b0d8c404efbaef3f645b054e32b4978999c5764
Brice Aminou <brice.aminou@gmail.com>
2024-10-30 17:19:51 +0100

    fix

--- prosody/custom/mod_transcriber_autostart.lua
+++ prosody/custom/mod_transcriber_autostart.lua
@@ -26,8 +26,8 @@ local function _start_recording(room, session, stanza)
 
     -- Invite Jigasi to the room to start transcription
     module:log("info", "Inviting Jigasi for transcription to room: %s", room.jid);
-    room:send(st.presence({ from = jigasi_jid, to = room.jid })
-        :tag("x", { xmlns = "http://jabber.org/protocol/muc" }));
+    -- room:send(st.presence({ from = jigasi_jid, to = room.jid })
+    --     :tag("x", { xmlns = "http://jabber.org/protocol/muc" }));
 
     -- Optionally send a message to indicate transcription has started
     room:send_message("Transcription service has been activated for this room.");
