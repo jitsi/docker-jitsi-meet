2c2152fc20a0c5bde6f51d7994b0c53df5ca340d
Brice Aminou <brice.aminou@gmail.com>
2024-10-30 16:46:15 +0100

    fix

--- prosody/custom/mod_transcriber_autostart.lua
+++ prosody/custom/mod_transcriber_autostart.lua
@@ -18,8 +18,9 @@ local function _start_recording(room, session)
     -- Customize Jigasi JID to the one set up in your environment
     local jigasi_jid = "jigasi@example.com"; -- replace with Jigasi's actual JID
 
-    module.log("info",room.jid)
-    module.log("info", jigasi_jid)
+    module:log("info",room.jid)
+    module:log("info", jigasi_jid)
+    module:log("Hello!!!")
     if not room then
         return
     end
