ca161794978ae3151c52dd73c5a9a32519164542
Brice Aminou <brice.aminou@gmail.com>
2024-11-01 20:23:26 +0100

    fix

--- web/config/custom-config.js
+++ web/config/custom-config.js
@@ -1,21 +1,21 @@
 config.toolbarButtons = [
   "microphone",
   "camera",
-  // "closedcaptions",
+  "closedcaptions",
   "desktop",
   "fullscreen",
   "fodeviceselection",
   "hangup",
-  // "profile",
+  "profile",
   "chat",
-  // "settings",
-  // "raisehand",
-  // "videoquality",
+  "settings",
+  "raisehand",
+  "videoquality",
   "filmstrip",
-  // "invite",
-  // "feedback",
-  // "shortcuts",
-  // "tileview",
+  "invite",
+  "feedback",
+  "shortcuts",
+  "tileview",
   "select-background",
   "participants-pane",
   "download",
@@ -25,12 +25,9 @@ config.toolbarButtons = [
   "security",
 ];
 
-config.premeetingBackground = 'url(https://t3.ftcdn.net/jpg/07/98/19/02/360_F_798190274_6VJG4eKmXlvtemnEYsmQOUHfhoF8dNbR.jpg)';
-
-// config.disableDeepLinking = true;
+config.disableDeepLinking = true;
 // config.transcription.enabled = true;
 // config.transcription.disableStartForAll = false;
 // // config.transcription.useAppLanguage = false;
 // // config.transcription.preferredLanguage = 'en-US';
 // config.transcription.autoCaptionOnTranscribe = true;
-// config.backgroundColor = "#FF0000"; 
--- web/config/custom-interface_config.js
+++ web/config/custom-interface_config.js
@@ -1,3 +1,2 @@
 // interfaceConfig.APP_NAME = "Stella Health Virtual"
 // interfaceConfig.transcription.disableStartForAll = false;
-// interfaceConfig.DISABLE_TRANSCRIPTION_SUBTITLES = true;
