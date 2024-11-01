e141e4480c354f1d4be679b86a3dff5124557622
Brice Aminou <brice.aminou@gmail.com>
2024-11-01 19:23:06 +0100

    fix

--- web/config/custom-config.js
+++ web/config/custom-config.js
@@ -1,34 +1,34 @@
-config.toolbarButtons = [
-  "microphone",
-  "camera",
-  "closedcaptions",
-  "desktop",
-  "fullscreen",
-  "fodeviceselection",
-  "hangup",
-  "profile",
-  "chat",
-  "settings",
-  "raisehand",
-  "videoquality",
-  "filmstrip",
-  "invite",
-  "feedback",
-  "shortcuts",
-  "tileview",
-  "select-background",
-  "participants-pane",
-  "download",
-  "help",
-  "mute-everyone",
-  "mute-video-everyone",
-  "security",
-];
+// config.toolbarButtons = [
+//   "microphone",
+//   "camera",
+//   "closedcaptions",
+//   "desktop",
+//   "fullscreen",
+//   "fodeviceselection",
+//   "hangup",
+//   "profile",
+//   "chat",
+//   "settings",
+//   "raisehand",
+//   "videoquality",
+//   "filmstrip",
+//   "invite",
+//   "feedback",
+//   "shortcuts",
+//   "tileview",
+//   "select-background",
+//   "participants-pane",
+//   "download",
+//   "help",
+//   "mute-everyone",
+//   "mute-video-everyone",
+//   "security",
+// ];
 
-config.disableDeepLinking = true;
-config.transcription.enabled = true;
-config.transcription.disableStartForAll = false;
-// config.transcription.useAppLanguage = false;
-// config.transcription.preferredLanguage = 'en-US';
-config.transcription.autoCaptionOnTranscribe = true;
-config.backgroundColor = "#FF0000"; 
+// config.disableDeepLinking = true;
+// config.transcription.enabled = true;
+// config.transcription.disableStartForAll = false;
+// // config.transcription.useAppLanguage = false;
+// // config.transcription.preferredLanguage = 'en-US';
+// config.transcription.autoCaptionOnTranscribe = true;
+// config.backgroundColor = "#FF0000"; 
--- web/config/custom-interface_config.js
+++ web/config/custom-interface_config.js
@@ -1,3 +1,3 @@
-interfaceConfig.APP_NAME = "Stella Health Virtual"
-interfaceConfig.transcription.disableStartForAll = false;
-interfaceConfig.DISABLE_TRANSCRIPTION_SUBTITLES = true;
+// interfaceConfig.APP_NAME = "Stella Health Virtual"
+// interfaceConfig.transcription.disableStartForAll = false;
+// interfaceConfig.DISABLE_TRANSCRIPTION_SUBTITLES = true;
