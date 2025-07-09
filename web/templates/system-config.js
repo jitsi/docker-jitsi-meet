3868c40636e8df414110de7035590a1619dd2dee
manohar <mallu@criticalstreams.com>
2025-07-09 15:22:28 +0530

    fix: Add DISABLE_HTTPS support for WebSocket URLs in system-config.js
    - Fix WebSocket connection failures when DISABLE_HTTPS=1 is set
    - WebSocket URLs now use 'ws://' when HTTPS is disabled and 'wss://' when enabled
    - Add volume mount for custom web templates to make changes persistent
    - Include all necessary default templates to ensure container compatibility
    
    Resolves WebSocket connection issues in local development environments
    where HTTPS is disabled but WebSocket was still trying to use WSS protocol.
    
    Changes:
    - docker-compose.yml: Add volume mount for ./web/templates:/defaults
    - web/templates/system-config.js: Add DISABLE_HTTPS environment variable support
    - web/templates/: Include all default Jitsi templates for container compatibility

---
+++ web/templates/system-config.js
@@ -0,0 +1,80 @@
+{{ $CONFIG_PREFIX := .Env.WEB_CONFIG_PREFIX | default "// Jitsi Meet configuration.\n" -}}
+{{ $BOSH_RELATIVE := .Env.BOSH_RELATIVE | default "false" | toBool -}}
+{{ $ENABLE_AUTH := .Env.ENABLE_AUTH | default "false" | toBool -}}
+{{ $ENABLE_AUTH_DOMAIN := .Env.ENABLE_AUTH_DOMAIN | default "true" | toBool -}}
+{{ $ENABLE_GUESTS := .Env.ENABLE_GUESTS | default "false" | toBool -}}
+{{ $ENABLE_SUBDOMAINS := .Env.ENABLE_SUBDOMAINS | default "true" | toBool -}}
+{{ $ENABLE_XMPP_WEBSOCKET := .Env.ENABLE_XMPP_WEBSOCKET | default "1" | toBool -}}
+{{ $DISABLE_HTTPS := .Env.DISABLE_HTTPS | default "false" | toBool -}}
+{{ $PUBLIC_URL_DOMAIN := .Env.PUBLIC_URL | default "https://localhost:8443" | trimPrefix "https://" | trimSuffix "/" -}}
+{{ $XMPP_AUTH_DOMAIN := .Env.XMPP_AUTH_DOMAIN | default "auth.meet.jitsi" -}}
+{{ $XMPP_DOMAIN := .Env.XMPP_DOMAIN | default "meet.jitsi" -}}
+{{ $XMPP_GUEST_DOMAIN := .Env.XMPP_GUEST_DOMAIN | default "guest.meet.jitsi" -}}
+{{ $XMPP_MUC_DOMAIN := .Env.XMPP_MUC_DOMAIN | default "muc.meet.jitsi" -}}
+{{ $XMPP_MUC_DOMAIN_PREFIX := (split "." $XMPP_MUC_DOMAIN)._0  -}}
+{{ $JVB_PREFER_SCTP := .Env.JVB_PREFER_SCTP | default "1" | toBool -}}
+
+{{ join "\n" (splitList "\\n" $CONFIG_PREFIX) }}
+var config = {};
+
+config.hosts = {};
+config.hosts.domain = '{{ $XMPP_DOMAIN }}';
+
+{{ if $ENABLE_SUBDOMAINS -}}
+var subdir = '<!--# echo var="subdir" default="" -->';
+var subdomain = '<!--# echo var="subdomain" default="" -->';
+if (subdir.startsWith('<!--')) {
+    subdir = '';
+}
+if (subdomain) {
+    subdomain = subdomain.substring(0,subdomain.length-1).split('.').join('_').toLowerCase() + '.';
+}
+config.hosts.muc = '{{ $XMPP_MUC_DOMAIN_PREFIX }}.' + subdomain + '{{ $XMPP_DOMAIN }}';
+{{ else -}}
+config.hosts.muc = '{{ $XMPP_MUC_DOMAIN }}';
+{{ end -}}
+
+{{ if $ENABLE_AUTH -}}
+{{ if $ENABLE_GUESTS -}}
+// When using authentication, domain for guest users.
+config.hosts.anonymousdomain = '{{ $XMPP_GUEST_DOMAIN }}';
+{{ end -}}
+{{ if $ENABLE_AUTH_DOMAIN -}}
+// Domain for authenticated users. Defaults to <domain>.
+config.hosts.authdomain = '{{ $XMPP_DOMAIN }}';
+{{ end -}}
+{{ end -}}
+
+{{ if $BOSH_RELATIVE -}}
+{{ if $ENABLE_SUBDOMAINS -}}
+config.bosh = '/'+ subdir + 'http-bind';
+{{ else -}}
+config.bosh = '/http-bind';
+{{ end -}}
+{{ else -}}
+{{ if $ENABLE_SUBDOMAINS -}}
+config.bosh = 'https://{{ $PUBLIC_URL_DOMAIN}}/' + subdir + 'http-bind';
+{{ else -}}
+config.bosh = 'https://{{ $PUBLIC_URL_DOMAIN}}/http-bind';
+{{ end -}}
+{{ end -}}
+
+{{ if $ENABLE_XMPP_WEBSOCKET -}}
+{{ if $DISABLE_HTTPS -}}
+{{ if $ENABLE_SUBDOMAINS -}}
+config.websocket = 'ws://{{ $PUBLIC_URL_DOMAIN }}/' + subdir + 'xmpp-websocket';
+{{ else -}}
+config.websocket = 'ws://{{ $PUBLIC_URL_DOMAIN }}/xmpp-websocket';
+{{ end -}}
+{{ else -}}
+{{ if $ENABLE_SUBDOMAINS -}}
+config.websocket = 'wss://{{ $PUBLIC_URL_DOMAIN }}/' + subdir + 'xmpp-websocket';
+{{ else -}}
+config.websocket = 'wss://{{ $PUBLIC_URL_DOMAIN }}/xmpp-websocket';
+{{ end -}}
+{{ end -}}
+{{ end -}}
+
+config.bridgeChannel = {
+    preferSctp: {{ $JVB_PREFER_SCTP }}
+};
