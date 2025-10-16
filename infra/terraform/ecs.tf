b9530ef720d13304a0e5dd16ceec1998da3903e3
Brice Aminou <brice.aminou@gmail.com>
2025-10-16 07:52:38 -0400

    load balancers removed

--- infra/terraform/ecs.tf
+++ infra/terraform/ecs.tf
@@ -1,19 +1,19 @@
 resource "aws_ecs_service" "jitsi" {
-  name            = "jitsi"
+  name            = var.name
   task_definition = aws_ecs_task_definition.jitsi.arn
   cluster         = var.cluster_name
 
-  load_balancer {
-    target_group_arn = aws_lb_target_group.jitsi.arn
-    container_name   = "web"
-    container_port   = 80
-  }
+  # load_balancer {
+  #   target_group_arn = aws_lb_target_group.jitsi.arn
+  #   container_name   = "web"
+  #   container_port   = 80
+  # }
 
-  load_balancer {
-    target_group_arn = aws_lb_target_group.jvb_udp_tg_ip.arn
-    container_name   = "jvb"
-    container_port   = 10000
-  }
+  # load_balancer {
+  #   target_group_arn = aws_lb_target_group.jvb_udp_tg_ip.arn
+  #   container_name   = "jvb"
+  #   container_port   = 10000
+  # }
 
   network_configuration {
     subnets         = var.private_subnets                 # e.g., public subnets used by your ALB/NLB
