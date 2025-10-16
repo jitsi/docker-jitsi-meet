22ed8cb94736b756d9823aef2bcd17a7db9ec27a
Brice Aminou <brice.aminou@gmail.com>
2025-10-16 08:06:42 -0400

    jvb lb added

--- infra/terraform/ecs.tf
+++ infra/terraform/ecs.tf
@@ -9,11 +9,11 @@ resource "aws_ecs_service" "jitsi" {
     container_port   = 80
   }
 
-  # load_balancer {
-  #   target_group_arn = aws_lb_target_group.jvb_udp_tg_ip.arn
-  #   container_name   = "jvb"
-  #   container_port   = 10000
-  # }
+  load_balancer {
+    target_group_arn = aws_lb_target_group.jvb_udp_tg_ip.arn
+    container_name   = "jvb"
+    container_port   = 10000
+  }
 
   network_configuration {
     subnets         = var.private_subnets                 # e.g., public subnets used by your ALB/NLB
