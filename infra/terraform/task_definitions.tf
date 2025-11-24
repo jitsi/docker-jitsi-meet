8d02eef38d91dbb3452b182d45a294ecced6431f
Brice Aminou <brice.aminou@gmail.com>
2025-11-24 12:24:48 -0500

    owner_restricted,lobby_autostart_on_owner,event_sync_component

--- infra/terraform/task_definitions.tf
+++ infra/terraform/task_definitions.tf
@@ -38,7 +38,7 @@ resource "aws_ecs_task_definition" "jitsi" {
     region                      = var.region
     version                     = var.jitsi_version
     public_url                  = var.url
-    prosody_plugins             = "token_security_ondemand,owner_restricted,event_sync_component"
+    prosody_plugins             = "owner_restricted,lobby_autostart_on_owner,event_sync_component"
     jvb_auth_password_arn       = aws_ssm_parameter.jvb_auth_password.arn
     jicofo_auth_password_arn    = aws_ssm_parameter.jicofo_auth_password.arn
     jicofo_component_secret_arn = aws_ssm_parameter.jicofo_component_secret.arn
