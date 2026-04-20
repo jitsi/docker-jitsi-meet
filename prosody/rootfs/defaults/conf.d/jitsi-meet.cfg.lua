416df923ade332774f7b215fba535801a6a308f1
Boris Grozev <boris@jitsi.org>
2026-04-20 16:29:11 -0500

    feat(prosody): support configurable anonymous_auth_methods for muc_resource_validate

--- prosody/rootfs/defaults/conf.d/jitsi-meet.cfg.lua
+++ prosody/rootfs/defaults/conf.d/jitsi-meet.cfg.lua
@@ -341,6 +341,9 @@ Component "{{ $XMPP_MUC_DOMAIN }}" "muc"
 
     {{ if $ENABLE_MUC_RESOURCE_VALIDATE -}}
     anonymous_strict = {{ if $MUC_RESOURCE_VALIDATE_ANONYMOUS_STRICT }}true{{ else }}false{{ end }};
+    {{ if .Env.PROSODY_MUC_RESOURCE_VALIDATE_ANON_METHODS -}}
+    anonymous_auth_methods = { "{{ join "\"; \"" (splitList "," .Env.PROSODY_MUC_RESOURCE_VALIDATE_ANON_METHODS | compact) }}" };
+    {{ end -}}
     {{ end -}}
     {{ if $ENABLE_RATE_LIMITS -}}
     -- Max allowed join/login rate in events per second.
