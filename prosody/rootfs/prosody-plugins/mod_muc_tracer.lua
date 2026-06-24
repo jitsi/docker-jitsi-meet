808e4296e044ad99416f682c9368e7ba4d2acccb
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2026-06-24 10:49:11 +0200

    Add parent context to prosody trace

--- prosody/rootfs/prosody-plugins/mod_muc_tracer.lua
+++ prosody/rootfs/prosody-plugins/mod_muc_tracer.lua
@@ -15,35 +15,37 @@ local function generate_span_id()
   return random_hex(8)
 end
 
+local function find_trace_extension(extensions)
+  for _, extension in ipairs(extensions) do
+    if extension.name == "trace" then
+      return extension
+    end
+  end
+  return nil
+end
+
 module:hook("muc-room-created", function(event)
+  local trace_ext = find_trace_extension(event.stanza.tags)
   local span = {
-    traceId = generate_trace_id(),
+    traceId = trace_ext and trace_ext.attr.trace or generate_trace_id(),
     spanId = generate_span_id(),
     name = "muc-room-created",
     kind = 2,
     startTimeUnixNano = string.format("%.0f", os.time() * 1e9),
-    endTimeUnixNano = string.format("%.0f", (os.time() + 1) * 1e9),
+    endTimeUnixNano = string.format("%.0f", os.time() * 1e9 + 1),
     status = { code = 1 },
-    attributes = {},
+    attributes = {
+      {
+        key = "name",
+        value = { stringValue = event.room.jid }
+      }
+
+    },
   }
-  table.insert(span.attributes,
-    {
-      key = "name",
-      value = { stringValue = event.room.jid }
-    }
-  )
-  table.insert(span.attributes,
-    {
-      key = "room",
-      value = { stringValue = json.encode(event.room) }
-    }
-  )
-  table.insert(span.attributes,
-    {
-      key = "stanza",
-      value = { stringValue = json.encode(event.stanza) }
-    }
-  )
+  if trace_ext then
+    span["parentSpanId"] = trace_ext.attr.parent
+  end
+
   local trace_data = {
     resourceSpans = {
       {
