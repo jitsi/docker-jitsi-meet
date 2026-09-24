b788f36f7433c23c3ab887f67fbd561ec8e6a7a6
Jaya Allamsetty <jaya.allamsetty@8x8.com>
2026-09-24 14:02:54 -0400

    feat(web): remove VIDEOQUALITY_*_SCALABILITY_MODE_ENABLED
    scalabilityModeEnabled is derived from browser support in lib-jitsi-meet and is
    no longer read from config.js (see jitsi/lib-jitsi-meet#3121). Drop the four
    VIDEOQUALITY_<codec>_SCALABILITY_MODE_ENABLED env passthroughs and their
    settings-config.js blocks. useSimulcast/useKSVC/bitrate overrides are unchanged.

--- web/rootfs/defaults/settings-config.js
+++ web/rootfs/defaults/settings-config.js
@@ -495,10 +495,6 @@ config.videoQuality.av1.maxBitratesVideo.ultraHd = {{ .Env.VIDEOQUALITY_BITRATE_
 config.videoQuality.av1.maxBitratesVideo.ssHigh = {{ .Env.VIDEOQUALITY_BITRATE_AV1_SS_HIGH }};
 {{ end -}}
 
-{{ if .Env.VIDEOQUALITY_AV1_SCALABILITY_MODE_ENABLED -}}
-config.videoQuality.av1.scalabilityModeEnabled = {{ .Env.VIDEOQUALITY_AV1_SCALABILITY_MODE_ENABLED | toBool }};
-{{ end -}}
-
 {{ if .Env.VIDEOQUALITY_AV1_USE_SIMULCAST -}}
 config.videoQuality.av1.useSimulcast = {{ .Env.VIDEOQUALITY_AV1_USE_SIMULCAST | toBool }};
 {{ end -}}
@@ -534,10 +530,6 @@ config.videoQuality.h264.maxBitratesVideo.ultraHd = {{ .Env.VIDEOQUALITY_BITRATE
 config.videoQuality.h264.maxBitratesVideo.ssHigh = {{ .Env.VIDEOQUALITY_BITRATE_H264_SS_HIGH }};
 {{ end -}}
 
-{{ if .Env.VIDEOQUALITY_H264_SCALABILITY_MODE_ENABLED -}}
-config.videoQuality.h264.scalabilityModeEnabled = {{ .Env.VIDEOQUALITY_H264_SCALABILITY_MODE_ENABLED | toBool }};
-{{ end -}}
-
 config.videoQuality.vp8 = {};
 config.videoQuality.vp8.maxBitratesVideo = {};
 
@@ -565,10 +557,6 @@ config.videoQuality.vp8.maxBitratesVideo.ultraHd = {{ .Env.VIDEOQUALITY_BITRATE_
 config.videoQuality.vp8.maxBitratesVideo.ssHigh = {{ .Env.VIDEOQUALITY_BITRATE_VP8_SS_HIGH }};
 {{ end -}}
 
-{{ if .Env.VIDEOQUALITY_VP8_SCALABILITY_MODE_ENABLED -}}
-config.videoQuality.vp8.scalabilityModeEnabled = {{ .Env.VIDEOQUALITY_VP8_SCALABILITY_MODE_ENABLED | toBool }};
-{{ end -}}
-
 config.videoQuality.vp9 = {};
 config.videoQuality.vp9.maxBitratesVideo = {};
 
@@ -594,10 +582,6 @@ config.videoQuality.vp9.maxBitratesVideo.ultraHd = {{ .Env.VIDEOQUALITY_BITRATE_
 
 {{ if .Env.VIDEOQUALITY_BITRATE_VP9_SS_HIGH }}
 config.videoQuality.vp9.maxBitratesVideo.ssHigh = {{ .Env.VIDEOQUALITY_BITRATE_VP9_SS_HIGH }};
-{{ end -}}
-
-{{ if .Env.VIDEOQUALITY_VP9_SCALABILITY_MODE_ENABLED -}}
-config.videoQuality.vp9.scalabilityModeEnabled = {{ .Env.VIDEOQUALITY_VP9_SCALABILITY_MODE_ENABLED | toBool }};
 {{ end -}}
 
 {{ if .Env.VIDEOQUALITY_VP9_USE_SIMULCAST -}}
